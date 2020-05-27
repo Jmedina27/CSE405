543ce39f6039fe53ae39d52e2c6f8174abbf0d58
Jesus Medina <005172852@coyote.csusb.edu>
2020-05-26 23:01:24 -0700

    adding updated firebase web app files

--- public/index.js
+++ public/index.js
@@ -1,10 +1,20 @@
+var userEmail = document.getElementById("email").value;
+//var rootRef = firebase.database().ref('Users');
+//rootRef.orderByChild('email').equalTo(userEmail).on("value", function(snapshot) {
+//    snapshot.forEach((function(child) { console.log(parent.key) }));
+//});
+
 firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
+        var userEmail = document.getElementById("email").value;
+        var fields = userEmail.split('@');
+        var userName = fields[0];
         document.getElementById("user").innerHTML = "You have signed in as " + user.email;
         document.getElementById("logged-in").style.display = "inline";
         document.getElementById("login").style.display = "none";
         console.log(user.displayName)
-        document.getElementBy
+        console.log(userName);
+        //document.getElementBy
  
         // User is signed in.
         if (user != null)  {
@@ -15,6 +25,7 @@ firebase.auth().onAuthStateChanged(function (user) {
         // No user is signed in.
     }
 });
+
 
 function login() {
     var userEmail = document.getElementById("email").value;
